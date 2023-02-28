ea9a49e55baf88ed2c20941381060a6b919e10f1
chung <chungjenching@gmail.com>
2023-02-28 20:32:35 +0800

    標註

--- app/src/main/java/com/simon/upload/utils/DarkMode.java
+++ app/src/main/java/com/simon/upload/utils/DarkMode.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 
 /**
- * 深色模式判斷, 分為 system 及 app, supporting dark-mode on target android 10 or above
+ * system 及 app 深色模式判斷, supporting dark-mode on target android 10 or above
  */
 public class DarkMode {
     private final Context context;
@@ -14,11 +14,17 @@ public class DarkMode {
         this.context = context;
     }
 
+    /**
+     * 判斷系統顯示畫面是 深色模式 or 淺色模式
+     */
     public boolean system() {
         UiModeManager uiModeManager = (UiModeManager) context.getSystemService(Context.UI_MODE_SERVICE);
         return uiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_YES;
     }
 
+    /**
+     * 判斷 app 顯示畫面是 深色模式 or 淺色模式
+     */
     public boolean app() {
         int uiMode = context.getResources().getConfiguration().uiMode;
         return (uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
